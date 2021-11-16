e5ef2480e6f1ecb07199b94ecaad3d77e1d6e02c
Tyler Fields <tyler@tylerfields.net>
2021-11-15 20:33:08 -0500

    Add git pages and github link

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -11,6 +11,9 @@ const App = () => (
     </div>
     {window.location.search.length ? <ParticipantPage /> : <OrganizerPage />}
     <img id="santa" alt="santa with beer" src={santaImg} />
+    <a className="git-link" href="https://github.com/tyler7771">
+      Created with love by Tyler Fields! Check out my Github. Cheers!
+    </a>
   </>
 );
 
